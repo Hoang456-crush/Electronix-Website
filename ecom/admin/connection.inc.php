be1be9b2d423efaf06a40f165a13b0002ae3ce78
Đỗ Đình Huy Hoàng <82626426+Hoang456-crush@users.noreply.github.com>
2023-08-10 07:03:15 +0700

    Update connection.inc.php

--- ecom/admin/connection.inc.php
+++ ecom/admin/connection.inc.php
@@ -2,7 +2,7 @@
 session_start();
 
 //Use the below $con command if you are deploying website using AWS RDS, EC2
-$con=mysqli_connect("database-1.crbww63n3dqm.ap-southeast-1.rds.amazonaws.com","ecom123","ecom$1234","ecomm");
+$con=mysqli_connect("database-1.crbww63n3dqm.ap-southeast-1.rds.amazonaws.com","ecom123","ecom$1234","ecom");
 
 //Use the below command if you want to run the website on Localhost i.e., XAMPP
 // $con=mysqli_connect("localhost","root","","ecom");
@@ -10,7 +10,7 @@ $con=mysqli_connect("database-1.crbww63n3dqm.ap-southeast-1.rds.amazonaws.com","
 define('SERVER_PATH',$_SERVER['DOCUMENT_ROOT'].'/Electronix-Website/ecom/');
 
 //Edit the IP address (127.0.0.1) with the Public IP address of your EC2 instance
-define('SITE_PATH','http://52.77.16.129/Electronix-Website/ecom/');
+define('SITE_PATH','http://18.140.43.215/Electronix-Website/ecom/');
 
 define('PRODUCT_IMAGE_SERVER_PATH',SERVER_PATH.'media/product/');
 define('PRODUCT_IMAGE_SITE_PATH',SITE_PATH.'media/product/');
